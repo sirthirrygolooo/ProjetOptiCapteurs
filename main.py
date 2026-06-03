bda7a471c6e8dd937a1d130169972271c1bf1bde
Timothée M. <timothee01meyer@gmail.com>
2026-06-03 08:35:28 +0200

    lecture fichier de test

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+import sys
+
 def main() -> int:
-    print("Hello, World!")
-    return 1
+    # fichier d'input
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <input_file>")
+        return 1
+    input_file = sys.argv[1]
+    
+    with open(input_file, 'r') as f:
+        data = f.read().strip().splitlines()
+        # nombre de capteurs
+        n_capteurs = int(data[0])
+        # nombre de zones
+        n_zones = int(data[1])
+        # zones surveillées par le capteur n
+        zones_par_capteur = []
+        for i in range(2, 2 + n_capteurs):
+            zones = list(map(int, data[i].split()))
+            zones_par_capteur.append(zones)
+            
+        print(f"Nombre de capteurs: {n_capteurs}")
+        print(f"Nombre de zones: {n_zones}")
+        print("Zones surveillées par chaque capteur:")
+        for i, zones in enumerate(zones_par_capteur):
+            print(f"Capteur {i+1}: {zones}")
+        
+
+    return 0
 
 if __name__ == "__main__":
     exit(main())
