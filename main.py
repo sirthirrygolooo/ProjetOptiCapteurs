67c0eb70bdc4f712b8dfe572ea9db28b96bf1f51
Timothée M. <timothee01meyer@gmail.com>
2026-06-03 08:44:44 +0200

    rectification durée de vie capteurs

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import sys
 
+
+def solveur(n_capteurs: int, n_zones: int, zones_par_capteur: list[list[int]], duree_de_vie_capteurs: list[int]) -> None:
+    return
+
+
 def main() -> int:
     # fichier d'input
     if len(sys.argv) != 2:
@@ -15,16 +20,22 @@ def main() -> int:
         n_zones = int(data[1])
         # zones surveillées par le capteur n
         zones_par_capteur = []
-        for i in range(2, 2 + n_capteurs):
+        # durée de vie des capteurs
+        duree_de_vie_capteurs = list(map(int, data[2].split()))
+        for i in range(3,3 + n_capteurs):
             zones = list(map(int, data[i].split()))
             zones_par_capteur.append(zones)
-            
+        
+        
         print(f"Nombre de capteurs: {n_capteurs}")
         print(f"Nombre de zones: {n_zones}")
         print("Zones surveillées par chaque capteur:")
         for i, zones in enumerate(zones_par_capteur):
             print(f"Capteur {i+1}: {zones}")
+        print(f"Durée de vie des capteurs: {duree_de_vie_capteurs}")
         
+        
+        solveur(n_capteurs, n_zones, zones_par_capteur, duree_de_vie_capteurs)
 
     return 0
 
